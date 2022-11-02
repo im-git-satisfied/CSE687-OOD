797705bfafe38840b102a5d48effdf2dc70cc617
Danny <im-git-satisfied@gmail.com>
2022-11-01 21:02:31 -0400

    update command line parser to create defaults, and handle a help message

--- Executive.cpp
+++ Executive.cpp
@@ -12,31 +12,94 @@ Executive::Executive(int argc, char *argv[], bool DEBUG) : count(argc), argv(arg
     }
 }
 
+// DEPRECATED: USE parse_args
 // Verify Count
 // ensure that the number of command line arguments are valid. 
+/*
 void Executive::verify_count(void){
+    std::string err_str = "ERROR >> Invalid number of arguments. See Help\n";
     if(DEBUG){
         std::cout << "DEBUG >> NUMBER ARGS: " << count << std::endl;
     }
     if (count != 4) {
         err = true;
-        Executive::err_out("ERROR >> Invalid number of arguments. See Help\n");
+        Executive::err_out(err_str);
+    }
+
+}
+*/
+
+void Executive::parse_args(void){
+
+    std::string err_str = "ERROR >> Invalid number of arguments. See Help\n";
+    int i = 1;
+    if(DEBUG){
+        std::cout << "DEBUG >> NUMBER ARGS: " << count << std::endl;
+    }
+
+    if (count == 1) {
+        err = true;
+        Executive::err_out(err_str);
+    }
+
+    for(i; i < count ; i++){
+        if(std::string(argv[1]) == "-h"){
+            Executive::err_out("");
+        }
+        if(std::string(argv[1]) == "-help"){
+            Executive::err_out("");
+        }
+        if(std::string(argv[1]) == "--h"){
+            Executive::err_out("");
+        }
+        if(std::string(argv[1]) == "--help"){
+            Executive::err_out("");
+        }
+    }
+
+    if(count == 2){
+        in_dir = argv[1];
+        temp_dir = DEFAULT_TEMP;
+        out_dir = DEFAULT_OUT;
+        std::cout << "INFO >> Using default directories 'TEMP_DIR' and 'OUT_DIR' for arguments temp_dir and out_dir" << std::endl;
+        return;
+    }
+
+    if(count == 3){
+        in_dir = argv[1];
+        temp_dir = argv[2];
+        out_dir = DEFAULT_OUT;
+        std::cout << "INFO >> Using default directory 'OUT_DIR' for argument out_dir" << std::endl;
+        return;
+    }
+
+    if(count == 4){
+        in_dir = argv[1];
+        temp_dir = argv[2];
+        out_dir = argv[3];
+        return;
     }
 
 }
 
 // print the help message 
 void Executive::print_help(void){
-    std::cout << "USAGE >> ./MapReduce in_dir temp_dir out_dir\n\n" << std::endl;
+    std::cout << "\tUSAGE >> " << argv[0] << " {in_dir} {temp_dir} {out_dir}\n" << std::endl;
+    std::cout << "\t-h, -help, --h, --help: print help message" << std::endl; 
+    std::cout << "\tin_dir: \t\t\REQUIRED: directory path containing files to be map reduced" << std::endl; 
+    std::cout << "\ttemp_dir: \t\tDEFAULT - TEMP_DIR: directory path to location of mapped/intermediary files. " << std::endl; 
+    std::cout << "\tout_dir: \t\tDEFAULT - OUT_DIR: directory path to location of reduced/final files. " << std::endl; 
 }
 
-
+// DEPRECATED: USE parse_args
 // access the directories passed in from the command line 
+/*
 void Executive::grab_dirs(void){
     in_dir = argv[1];
     temp_dir = argv[2];
     out_dir = argv[3];
 }
+*/
 
 // err_out if command the wrong number of arguments are passed 
 void Executive::err_out(std::string err_msg) {
@@ -47,8 +110,9 @@ void Executive::err_out(std::string err_msg) {
 
 int Executive::start() {
 
-    verify_count();         //verify dirs, err out if it's the wrong number 
-    grab_dirs();            // grab the directories, used to pass to Workflow
+    parse_args();             // replaces verify_count and grab_dirs. 
+    //verify_count();         //verify dirs, err out if it's the wrong number 
+    //grab_dirs();            // grab the directories, used to pass to Workflow
 
     // create a Workflow class
     worker = new Workflow(in_dir, temp_dir, out_dir, DEBUG);
--- Executive.h
+++ Executive.h
@@ -16,11 +16,14 @@ class Executive
     private:
         
         int count;
+        std::string DEFAULT_TEMP = "TEMP_DIR";
+        std::string DEFAULT_OUT = "OUT_DIR";
         std::string in_dir;
         std::string temp_dir;
         std::string out_dir;
         Workflow *worker;
         bool DEBUG;
+
 
         bool err;
         char **argv;
@@ -30,13 +33,13 @@ class Executive
         explicit Executive();
         explicit Executive(int argc, char** argv, bool DEBUG);
 
-        
+        void parse_args(void);
 
-        void verify_count(void);
+        //void verify_count(void);
 
         void print_help(void);
 
-        void grab_dirs(void);
+        //void grab_dirs(void);
 
         void err_out(std::string);
 
